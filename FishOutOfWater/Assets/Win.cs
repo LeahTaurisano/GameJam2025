933d4bdd758664d8045503819f7dd2866dc277a4
kim2057 <126519201+LavrentyevIvan@users.noreply.github.com>
2025-01-26 12:21:18 +0000

    WinScreen implemented
    (Had to run to plane please move the 2 fish back to the start, goodluck team! T_T)

---
+++ FishOutOfWater/Assets/Win.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
