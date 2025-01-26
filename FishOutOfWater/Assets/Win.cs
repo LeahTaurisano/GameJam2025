aa4f385e101abcf47c1745571f062f2ab84b42bc
NicholasOkovic <139954443+NicholasOkovic@users.noreply.github.com>
2025-01-26 15:26:50 -0800

    BubbleTransition

--- FishOutOfWater/Assets/Win.cs
+++ FishOutOfWater/Assets/Win.cs
@@ -1,13 +1,25 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Win : MonoBehaviour
 {
+    [SerializeField] private GameObject BubbleTransition;
+    [SerializeField] private AudioClip BubbleClip;
+    [SerializeField] private float DelayedLoadTime;
+    [SerializeField] private float volume;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene(2);
+            BubbleTransition.gameObject.SetActive(true);
+            AudioManager.instance.PlayGlobalSoundEffect(BubbleClip, volume, false);
+            StartCoroutine(DelayedLoad(DelayedLoadTime));
         }
+    }
+    IEnumerator DelayedLoad(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene(2);
     }
 }
